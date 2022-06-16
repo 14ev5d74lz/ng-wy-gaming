ec95a2c248dfebd012be80eb590695108e95ea84
14ev5d74lz <14ev5d74lz@protonmail.com>
2022-06-16 12:51:35 +0800

    Update prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  language: "en",
+  companyName: "Wy Gaming",
+  companyEmail: "wygaming@gmail.com",
+  companyPhone: "03-9999999",
+  companyWorkingHour: {
+    start: "12pm",
+    end: "10pm"
+  }
 };
